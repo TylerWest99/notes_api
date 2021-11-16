aeaf9d73a7cc0a48d51540a683e651b6a88d9aab
Tyler West <tylerwest@Tylers-MacBook-Pro.local>
2021-11-16 15:53:59 -0500

    adds stuff

---
+++ user.js
@@ -0,0 +1,49 @@
+// user.js file for user authentication
+
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const pbkdf2 = require('pbkdf2');
+const { Schema } = mongoose;
+
+// Connect to the database
+// CHANGE THIS TO YOUR DB CONNECTION UNLESS YOU ARE TESTING AGAINST MINE.
+const uri = "mongodb+srv://user:user@cluster0.pnwwl.mongodb.net/myData?retryWrites=true&w=majority"
+try {
+	mongoose.connect(uri);
+} catch (err){
+	console.log(err);
+}
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	salt: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+// BE SURE TO CHANGE THIS TO YOUR DESIRED PASSWORD.
+let clearText = "Tyler";
+let salt = crypto.randomBytes(32).toString('hex');
+let password = pbkdf2.pbkdf2Sync(clearText, salt, 1, 32, 'sha512').toString('hex');
+
+// CHANGE 'MRW' TO THE USERNAME YOU WISH TO USE.
+const User = mongoose.model('User', userSchema);
+let add = new User({
+	username: 'Tyler',
+	password: password,
+	salt: salt
+});
+
+add.save();
