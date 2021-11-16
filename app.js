f071f79de0be4b4ee19638c0a7b54e7a8283b4a9
Tyler West <tylerwest@Tylers-MacBook-Pro.local>
2021-11-16 17:15:02 -0500

    upates

--- app.js
+++ app.js
@@ -15,8 +15,68 @@ const express = require('express');
 const mongoose = require('mongoose');
 const note = require('./note');
 
+const passport = require('passport')
+const Strategy = require('passport-http').BasicStrategy;
+const pbkdf2 = require('pbkdf2')
+const { Schema } = mongoose;
+
+//Auth 
+//Authentication
+const userSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	salt: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+const User = mongoose.model('User', userSchema);
+
+passport.use(new Strategy(
+	function(username, password, done){
+		username.findOne({ username: username }, function(err, user){
+			//cant connect db
+			if(err){
+				return done(err);
+			}
+			if(!user){
+				console.log("No user found")
+				return done(null, false);
+			}
+
+			if(!validPassword(password, user.salt, user.password)){
+				console.log('Wrong password')
+				return done(null, false);
+			}
+
+			return done(null, user)
+		});
+	}
+));
+
+const validPassword = function(password, salt, hash){
+	let key = pbkdf2.pbkdf2Sync(password, salt, 1, 32, 'sha512');
+
+	if(key.toString('hex') != hash){
+		return false;
+	}
+	return true;
+}
+
 // Connect to the database
-const uri = 'mongodb+srv://mrwoodring:BadPassword@cluster0.dxk6b.mongodb.net/XUniversity?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://user:user@cluster0.pnwwl.mongodb.net/notesapi?retryWrites=true&w=majority'
 try {
 	mongoose.connect(uri);
 } catch (err){
@@ -26,6 +86,7 @@ try {
 // Create the app instance
 const app = express();
 const port = 8080;
+
 
 module.exports = { app, mongoose };
 
@@ -41,7 +102,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/notes', note.getAll);
-app.get('/notes/:searchTerm', note.getOne);
+app.get('/notes/:searchTerm', checkAuth, note.getOne);
 app.post('/notes', note.postOne);
 app.delete('/notes/:objectId', note.deleteOne);
 app.put('/notes/:objectId', note.putOne);
--- user.js
+++ user.js
@@ -5,9 +5,12 @@ const mongoose = require('mongoose');
 const pbkdf2 = require('pbkdf2');
 const { Schema } = mongoose;
 
+const passport = require('passport')
+const Srategy = require('password-http').BasicStrategy;
+
 // Connect to the database
 // CHANGE THIS TO YOUR DB CONNECTION UNLESS YOU ARE TESTING AGAINST MINE.
-const uri = "mongodb+srv://user:user@cluster0.pnwwl.mongodb.net/myData?retryWrites=true&w=majority"
+const uri = "mongodb+srv://user:user@cluster0.pnwwl.mongodb.net/notesapi?retryWrites=true&w=majority"
 try {
 	mongoose.connect(uri);
 } catch (err){
@@ -34,16 +37,48 @@ const userSchema = new Schema({
 });
 
 // BE SURE TO CHANGE THIS TO YOUR DESIRED PASSWORD.
-let clearText = "Tyler";
+let clearText = "51Rebel";
 let salt = crypto.randomBytes(32).toString('hex');
 let password = pbkdf2.pbkdf2Sync(clearText, salt, 1, 32, 'sha512').toString('hex');
 
 // CHANGE 'MRW' TO THE USERNAME YOU WISH TO USE.
 const User = mongoose.model('User', userSchema);
 let add = new User({
-	username: 'Tyler',
+	username: 'TylerRebel',
 	password: password,
 	salt: salt
 });
 
-add.save();
+//Authentication
+passport.use(new Strategy(
+	function(username, password, done){
+		username.findOne({ username: username }, function(err, user){
+			//cant connect db
+			if(err){
+				return done(err);
+			}
+			if(!user){
+				console.log("No user found")
+				return done(null, false);
+			}
+
+			if(!validPassword(password, user.salt, user.password)){
+				console.log('Wrong password')
+				return done(null, false);
+			}
+
+			return done(null, user)
+		});
+	}
+));
+
+const validPassword = function(password, salt, hash){
+	let key = pbkdf2.pbkdf2Sync(password, salt, 1, 32, 'sha512');
+
+	if(key.toString('hex') != hash){
+		return false;
+	}
+	return true;
+}
+
+// add.save();
